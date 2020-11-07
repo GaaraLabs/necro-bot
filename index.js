8702e8606e20bfabc27f166499f29b466e8bfdad
micky0singh <38345454+micky0singh@users.noreply.github.com>
2020-11-08 00:46:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ client.loadCommands();
 client.on('ready', () => {
   console.log(`READY Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
   client.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`, "Ready", "event");
-  client.user.setActivity(`Hey there i'am using discord.`);
+  client.user.setActivity(`Serving Micky with ${client.users.size} Members`);
 });;
 
 client.on('error', error => {
