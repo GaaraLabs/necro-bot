9ad81cc61f968a68a1824431a4cee63a39d2d452
micky0singh <38345454+micky0singh@users.noreply.github.com>
2020-11-08 00:23:34 +0530

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,17 +1,20 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
     let bicon = bot.user.displayAvatarURL;
     let botembed = new Discord.RichEmbed()
-    .setDescription("Bot Information")
-    .setColor("#15f153")
+    .setTitle("__**Information**__")
+    .setColor(0x00ffff)
     .setThumbnail(bicon)
-    .addField("Bot Name", bot.user.username)
-    .addField("Created On", bot.user.createdAt);
+    .setDescription("**Basic Information About Bot**")
+    .addField("**Bot Name**", bot.user.username)
+    .addField("**Developer**", "<@612803327675334673>")
+  message.channel.send(botembed);
+};
 
-    message.channel.send(botembed);
-}
-
-module.exports.help = {
-  name:"botinfo"
-}
+exports.help = {
+  name: "info",
+  category: "General",
+  description: "Get some information about me.",
+  usage: ",info"
+};
