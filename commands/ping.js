9390c034dc47f027f04d82e507245a257387f754
micky0singh <38345454+micky0singh@users.noreply.github.com>
2020-11-17 20:32:25 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -8,7 +8,7 @@ module.exports.run = async (client, message, args) => {
   message.channel.send(embed).then(m => {
   	embed
      	.setTitle("Pong!")
-  		.setDescription("Latency is " + (m.createdTimestamp - message.createdTimestamp) + "ms. API latency is " + Math.round(client.ping) + "ms.");
+  		.setDescription("${message.author} Ye le Bhosdk ye teri ping " + (m.createdTimestamp - message.createdTimestamp) + "ms. Ye meri ping madarchod tere se fast hu me " + Math.round(client.ping) + "ms.");
 
       m.edit(embed);
   });
