0b4a4442fbd87494e6e939735ca7a024233ff90f
micky0singh <38345454+micky0singh@users.noreply.github.com>
2020-11-22 04:24:27 +0530

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let bicon = bot.user.displayAvatarURL;
+    let botembed = new Discord.RichEmbed()
+    .setColor('#0099ff')
+      .setTitle('Click here to invite me to your server.')
+      .setURL('https://discord.com/oauth2/authorize?client_id=768058720621821954&permissions=1037429846&scope=bot')
+      .setTimestamp()
+  
+    message.channel.send(botembed);
+}
+
+exports.help = {
+  name: "invite",
+  category: "General",
+  description: "Gives bot invite link",
+  usage: ",invite"
+};
