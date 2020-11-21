f20f9b70598c99a8fe16b1163c3e310da54eebef
micky0singh <38345454+micky0singh@users.noreply.github.com>
2020-11-22 03:35:50 +0530

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -4,7 +4,15 @@ const errors = require("../utils/errors.js");
 module.exports.run = async (bot, message, args) => {
 
   message.delete();
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+    let owners = process.env.OWNER.split(',');
+  
+  if (!owners.includes(message.author.id))  {
+    embed
+      .setTitle("Permission Denied")
+      .setDescription("You do not have permission to use this command. It's for owners only.");
+    
+    return message.channel.send(embed);
+  }
   let botmessage = args.join(" ");
   message.channel.send(botmessage);
 }
