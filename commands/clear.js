4d6f7ae3b001f794ac2adc864766cc8e3e8472b9
micky0singh <38345454+micky0singh@users.noreply.github.com>
2020-11-24 03:37:46 +0530

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -3,7 +3,12 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+    let owners = process.env.OWNER.split(',');
+  
+  if (!owners.includes(message.author.id))  {
+    embed
+      .setTitle("Permission Denied")
+      .setDescription("You do not have permission to use this command. It's for owners only.");
   if(!args[0]) return message.channel.send("oof");
   message.channel.bulkDelete(args[0]).then(() => {
     message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
