2f00376302bd0be728d3a1eeece80b3896f6cd91
micky0singh <38345454+micky0singh@users.noreply.github.com>
2020-12-05 01:14:00 +0530

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -5,8 +5,7 @@ module.exports.run = async (bot, message, args) => {
     let botembed = new Discord.RichEmbed()
     .setColor('#0099ff')
       .setTitle('Click here to invite me to your server')
-      .setURL('https://discord.com/oauth2/authorize?client_id=768058720621821954&permissions=1037429846&scope=bot')
-      .setTimestamp()
+      .setURL('https://discord.com/oauth2/authorize?client_id=768058720621821954&scope=bot&permissions=2146827775')
   
     message.channel.send(botembed);
 }
